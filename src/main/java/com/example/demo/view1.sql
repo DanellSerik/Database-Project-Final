95f167a117d49e4de79025a5512f8a53c6645397
Danell <serikdanell@gmail.com>
2025-04-02 17:16:40 -0400

    fixed view1

--- src/main/java/com/example/demo/view1.sql
+++ src/main/java/com/example/demo/view1.sql
@@ -1,24 +1,21 @@
-create view area_avail as
-select
-    h.address->>'city' as city,
-    r.room_num,
+CREATE OR REPLACE VIEW area_avail AS
+SELECT
+    h.address->>'city' AS city,
     r.hotel_id,
-    count(r.room_num) as avail_rooms
-from
+    COUNT(r.room_num) AS avail_rooms
+FROM
     hotel h
-        join
-    room r on h.hotel_id = r.hotel_id
-        left join
-    booking ba on r.hotel_id = ba.hotel_id
-        and r.room_num = ba.room_num
-        left join
-    renting ra on r.hotel_id = ra.hotel_id
-        and r.room_num = ra.room_num
-where
-    (ba.booking_id is null or
-     (ba.check_out_date <= current_date or ba.check_in_date >= current_date))
-  and
-    (ra.renting_id is null or
-     (ra.check_out_date <= current_date or ra.check_in_date >= current_date))
-group by
-    city, r.room_num, h.hotel_id;
+        JOIN
+    room r ON h.hotel_id = r.hotel_id
+        LEFT JOIN
+    booking ba ON r.hotel_id = ba.hotel_id AND r.room_num = ba.room_num
+        LEFT JOIN
+    renting ra ON r.hotel_id = ra.hotel_id AND r.room_num = ra.room_num
+WHERE
+    (ba.booking_id IS NULL OR
+     (ba.check_out_date <= CURRENT_DATE OR ba.check_in_date >= CURRENT_DATE))
+  AND
+    (ra.renting_id IS NULL OR
+     (ra.check_out_date <= CURRENT_DATE OR ra.check_in_date >= CURRENT_DATE))
+GROUP BY
+    city, r.hotel_id;
