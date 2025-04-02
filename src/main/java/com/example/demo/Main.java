571ae1478affb7206a19e4836da633a7d75cc314
Danell <serikdanell@gmail.com>
2025-04-01 22:17:06 -0400

    added populate.sql

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -11,11 +11,5 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
         SpringApplication.run(Main.class, args);
-        SignInService signInService = new SignInService();
-        Address address = new Address("dsadsaas", "sdads", "dsadsa", "dsadsa", "dsadsads");
-        long now = System.currentTimeMillis();
-        Date date = new Date(now);
-        Customer customer = new Customer("shdsiodjosdjoisa", "Drivers License", "sdassa", "sasad", "dsadsa", address, date);
-        signInService.addCustomer(customer);
     }
 }
